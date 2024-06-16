513f7884d85848b3ba6a816d7143a6447b752723
Pierre Rubens Milorme <prubensmilorme@gmail.com>
2024-06-16 17:16:20 -0400

    add react version for the ui, rename old ui version as angular version

---
+++ lakaypam-ui-react/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <diV className="app w-full h-screen bg-red-100 flex justify-center items-center">
+      <h1 className="text-3xl font-bold">
+        LakayPam React Version
+      </h1>
+    </diV>
+  )
+}
