57b39fdbe7700287dd053fca066e35e35841db11
Pierre Rubens Milorme <prubensmilorme@gmail.com>
2024-06-16 21:51:15 -0400

    adding logo

--- lakaypam-ui-react/src/App.jsx
+++ lakaypam-ui-react/src/App.jsx
@@ -1,9 +1,9 @@
+import Navbar from "./components/navbar/Navbar"
+
 export default function App() {
   return (
-    <diV className="app w-full h-screen bg-red-100 flex justify-center items-center">
-      <h1 className="text-3xl font-bold">
-        LakayPam React Version
-      </h1>
+    <diV className="app w-full h-screen bg-red-100">
+        <Navbar />
     </diV>
   )
 }
